23cb3dda57edd8b2d13a68b623e13e454280b205
Florent SYX <fsyx@vinatis.com>
2024-05-03 16:08:57 +0200

    tt

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let inputText = "";
+  let outputText = "";
+
+  function removeNullBytes() {
+    outputText = inputText.replace(/\u000B/g, " ");
+    navigator.clipboard.writeText(outputText);
+  }
+</script>
+
+<style>
+  textarea {
+    width: 300px;
+    height: 200px;
+    margin-bottom: 10px;
+  }
+  button {
+    padding: 8px 16px;
+    background-color: #4caf50;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+</style>
+
+<textarea bind:value={inputText}></textarea>
+<button on:click={removeNullBytes}>Remove Null Bytes & Copy</button>
+<pre style="text-align: left;">{outputText}</pre>
